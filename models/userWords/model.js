5e3f053c8c36922d464377377fd60d15742bb95d
minung.han <hmu332233@gmail.com>
2019-12-03 01:05:52 +0900

    feat: UserWord model에 block, params 추가

--- models/userWords/model.js
+++ models/userWords/model.js
@@ -1,5 +1,7 @@
-const { db } = require('./info');
+const { db, CONSTANTS } = require('./info');
 
-exports.create = ({ id, content }) => {
-  return db.user_words.create({ id, content });
+exports.CONSTANTS = CONSTANTS;
+
+exports.create = ({ origin, id, utterance, block, params }) => {
+  return db.user_words.create({ origin, id, utterance, block, params });
 };
--- models/userWords/model.test.js
+++ models/userWords/model.test.js
@@ -1,29 +1,46 @@
 const { setupDB } = require('./../../jest.setup-db');
 setupDB();
 
-const { db } = require('./info');
+const { db, CONSTANTS } = require('./info');
 const UserWords = require('./model');
 
 describe('models - userWords', () => {
+  describe('CONSTANTS', () => {
+    test('CONSTANTS가 model에 정상적으로 있음', () => {
+      expect(UserWords.CONSTANTS).toEqual(CONSTANTS);
+    });
+  });
+
   describe('create', () => {
     test('id 값이 없을 경우 에러', async () => {
       try {
-        await UserWords.create({ content: 'test ' });
+        await UserWords.create({ origin: UserWords.CONSTANTS.ORIGIN.KAKAO, utterance: 'test ' });
         expect(true).toBe(false);
       } catch (err) {
         expect(err.name).toBe('ValidationError');
       }
     });
 
-    test('content 값이 없을 경우 ""', async () => {
-      const userWord = await UserWords.create({ id: 'test' });
-      expect(userWord.content).toBe('');
+    test('origin 값이 없을 경우 kakao', async () => {
+      try {
+        await UserWords.create({ id: 'test' });
+        expect(true).toBe(false);
+      } catch (err) {
+        expect(err.name).toBe('ValidationError');
+      }
     });
 
-    test('id와 content가 있을 경우 정상 처리', async () => {
-      const userWord = await UserWords.create({ id: 'test', content: 'test' });
+    test('utterance 값이 없을 경우 ""', async () => {
+      const userWord = await UserWords.create({ origin: UserWords.CONSTANTS.ORIGIN.KAKAO, id: 'test' });
+      expect(userWord.origin).toBe(UserWords.CONSTANTS.ORIGIN.KAKAO);
+      expect(userWord.utterance).toBe('');
+    });
+
+    test('origin, id, utterance 있을 경우 정상 처리', async () => {
+      const userWord = await UserWords.create({ origin: UserWords.CONSTANTS.ORIGIN.KAKAO, id: 'test', utterance: 'test' });
+      expect(userWord.origin).toBe(UserWords.CONSTANTS.ORIGIN.KAKAO);
       expect(userWord.id).toBe('test');
-      expect(userWord.content).toBe('test');
+      expect(userWord.utterance).toBe('test');
     });
   });
 });
