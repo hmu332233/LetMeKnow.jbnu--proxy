40efb6ac441e266843a4fac7115cde6e7194e652
minung.han <hmu332233@gmail.com>
2019-12-10 02:10:18 +0900

    feat(kakao): context가 ''이면 utterance를 전송하도록 수정

--- controllers/kakao/index.js
+++ controllers/kakao/index.js
@@ -3,8 +3,8 @@ const parser = require('../../utils/parser');
 
 exports.message = async (req, res, next) => {
   try {
-    const { userId, context } = parser.parseKakaoRequestBody(req.body);
-    const coreResponse = await coreApi.message({ userId, utterance: context });
+    const { userId, utterance, context } = parser.parseKakaoRequestBody(req.body);
+    const coreResponse = await coreApi.message({ userId, utterance: context || utterance });
     res.json(coreResponse);
   } catch (err) {
     next(err);
