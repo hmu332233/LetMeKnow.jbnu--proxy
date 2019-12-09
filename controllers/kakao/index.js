1643edd35e3dad6cf9794b3af3b7244b63af4abc
minung.han <hmu332233@gmail.com>
2019-12-10 00:54:32 +0900

    feat: kakao 전송 전 utterance 변환 후 전송하도록 수정

--- controllers/kakao/index.js
+++ controllers/kakao/index.js
@@ -2,9 +2,10 @@ const coreApi = require('../../utils/api/coreApi');
 const parser = require('../../utils/parser');
 
 exports.message = async (req, res, next) => {
-  const { userId, utterance } = parser.parseKakaoRequestBody(req.body);
   try {
-    const coreResponse = await coreApi.message({ userId, utterance });
+    const { userId, params } = parser.parseKakaoRequestBody(req.body);
+    const newUtterance = Object.values(params).join(' ');
+    const coreResponse = await coreApi.message({ userId, utterance: newUtterance });
     res.json(coreResponse);
   } catch (err) {
     next(err);
